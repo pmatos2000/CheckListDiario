966c52e9f7874b5b7d1b226a09fbd3ba28d5d21e
pmatos2000 <pmatos2000@gmail.com>
2021-04-02 16:32:46 -0300

    Criação do método para chamar a activity

--- app/src/main/java/br/pmatos/checklistdiario/CheckListActivity.kt
+++ app/src/main/java/br/pmatos/checklistdiario/CheckListActivity.kt
@@ -1,12 +1,14 @@
 package br.pmatos.checklistdiario
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class CheckListActivity : AppCompatActivity() {
 
@@ -19,11 +21,15 @@ class CheckListActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
+                setOf(
+                        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        finish()
     }
 }
